9542f81e79bfda33197477070de09dcaa39cb3ec
AndrHend062 <andrhend062@ed.amdsb.ca>
2017-11-09 12:22:14 -0500

    Add project files.

---
+++ Adventure game/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Adventure_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.debugLabel = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(438, 207);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "you are a bear what do you want to do go to river or the woods";
+            // 
+            // label1
+            // 
+            this.label1.Image = global::Adventure_game.Properties.Resources.blue_50x50;
+            this.label1.Location = new System.Drawing.Point(12, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 62);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Image = global::Adventure_game.Properties.Resources.red_50x50;
+            this.label2.Location = new System.Drawing.Point(12, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 62);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Image = global::Adventure_game.Properties.Resources.green_50x50;
+            this.label3.Location = new System.Drawing.Point(185, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 62);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Image = global::Adventure_game.Properties.Resources.yellow_50x50;
+            this.label4.Location = new System.Drawing.Point(185, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 62);
+            this.label4.TabIndex = 4;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Location = new System.Drawing.Point(94, 216);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(85, 62);
+            this.blueLabel.TabIndex = 5;
+            this.blueLabel.Text = "woods B";
+            // 
+            // redLabel
+            // 
+            this.redLabel.Location = new System.Drawing.Point(93, 308);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(85, 62);
+            this.redLabel.TabIndex = 6;
+            this.redLabel.Text = "river M";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.Location = new System.Drawing.Point(266, 216);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(85, 62);
+            this.greenLabel.TabIndex = 7;
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.Location = new System.Drawing.Point(266, 308);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(85, 62);
+            this.yellowLabel.TabIndex = 8;
+            // 
+            // debugLabel
+            // 
+            this.debugLabel.ForeColor = System.Drawing.Color.Red;
+            this.debugLabel.Location = new System.Drawing.Point(370, 347);
+            this.debugLabel.Name = "debugLabel";
+            this.debugLabel.Size = new System.Drawing.Size(100, 23);
+            this.debugLabel.TabIndex = 9;
+            this.debugLabel.Text = "0";
+            this.debugLabel.Visible = false;
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.ForeColor = System.Drawing.Color.Red;
+            this.dayLabel.Location = new System.Drawing.Point(14, 193);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(100, 23);
+            this.dayLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 379);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.debugLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.Label debugLabel;
+        private System.Windows.Forms.Label dayLabel;
+    }
+}
+
---
+++ Adventure game/Form1.cs
@@ -0,0 +1,265 @@
+﻿//adventure game andrew henderson
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Adventure_game
+{
+    public partial class Form1 : Form
+    {        Random randGen = new  Random() ;
+        int scene = 0;
+        int day = 0;
+        int Starve = 0 ;
+
+        public Form1()
+        {
+            InitializeComponent();
+            debugLabel.Text = scene + "";
+            dayLabel.Text = day + "";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            int rand = randGen.Next(1, 10);
+            /// to the next appropriate scene
+            
+            
+           if (e.KeyCode == Keys.M)     //red button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 4) { scene = 3; }
+                else if (scene == 3) { scene = 5; }
+                else if (scene == 5) { scene = 99; }
+                else if (scene == 8) { scene = 99; }
+                else if (scene == 11) { scene = 99; }
+                else if (scene == 15) { scene = 99; }
+                else if (scene == 10) { scene = 100; }
+                else if (scene == 7) { scene = 100; }
+                else if (scene == 16) { scene = 100; }
+                else if (scene == 18) { scene = 100; }
+                else if (scene == 6)
+                {
+                    if (rand > 90) { scene = 8; }
+                    else { scene = 9; }
+                }
+                else if (scene == 9) { scene = 11; }
+                else if (scene == 10) { scene = 100; }
+                else if (scene == 14) { scene = 100; }
+                else if (scene == 2) { scene = 12; }
+                else if (scene == 12) { scene = 13; }
+                else if (scene == 17) { scene = 15; }
+                else if (scene == 11) { scene = 1; }
+                else if (scene == 99) { scene = 0; }
+                else if (scene == 100) { scene = 0; }
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0) { scene = 2; }
+                else if (scene == 1) { scene = 4; }
+                else if (scene == 3) { scene = 6; }
+                else if (scene == 4) { scene = 7; }
+                else if (scene == 6) { scene = 8; }
+                else if (scene == 9) { scene = 10; }
+                else if (scene == 2) { scene = 20; }
+                else if (scene == 13) { scene = 15; }
+                else if (scene == 12) { scene = 1; }
+                else if (scene == 20) { scene = 17; }
+                else if (scene == 17) { scene = 18; }
+                else if (scene == 100) { scene = 101; }
+            }
+            else if (e.KeyCode == Keys.N)  //green button press
+            {
+                if (scene == 12) { scene = 14; }
+              
+            }
+            else if (e.KeyCode == Keys.Tab)  //green button press
+            {
+                debugLabel.Visible = true;
+
+            }
+
+            debugLabel.Text = scene + " debug";
+            dayLabel.Text = "day "+day  ;
+
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "you are a bear what do you want to do \n go to river or the woods";
+                    redLabel.Text = "river ";
+                    blueLabel.Text = "woods ";
+                    break;
+
+                case 1:
+                    outputLabel.Text = "you approach a river \n do you try and catch fish " +
+                        "or cross the river";
+                      redLabel.Text = "cross ";
+                    blueLabel.Text = "catch fish ";
+                    break;
+
+                case 2:
+                    outputLabel.Text = "you wander in the wood you see a deer " +
+                        "\n do you charge or sneak up on it ";
+                      redLabel.Text = "sneek  ";
+                    blueLabel.Text = "charge ";
+                    break;
+
+                case 3:
+                    outputLabel.Text = "you cross the river and you see a pack of monkeys charging" +
+                        " you do you run or attack";
+                      redLabel.Text = "run ";
+                    blueLabel.Text = "attack ";
+                    break;
+
+                case 4:
+                    outputLabel.Text = "you catch 3 fish  \n do you stay " +
+                        "and catch more or cross";
+                      redLabel.Text = "cross ";
+                    blueLabel.Text = "catch more ";
+                    break;
+
+                case 5:
+                    outputLabel.Text = "they are faster than you and" +
+                        " you are overwelmed and die ";
+                      redLabel.Text = "river ";
+                    blueLabel.Text = "woods ";
+                    break;
+
+                case 6:
+                    outputLabel.Text = "you stand on your back legs. \n" +
+                        " do you windmill menuver or dropkick ";
+                    redLabel.Text = "windmill ";
+                    blueLabel.Text = "dropkick ";
+                    break;
+                case 7:
+                    outputLabel.Text = "you catch 3 more fish you survive";
+                    redLabel.Text = "continue  ";
+                    blueLabel.Text = " ";
+                    break;
+                    
+
+                case 8:
+                    outputLabel.Text = "you get overwhlmed by monkeys and die  ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = " ";
+                    break;
+                    
+
+                case 9:
+                    outputLabel.Text = "you defeat all the monkeys \n do you hunt or sleep ";
+                    redLabel.Text = "hunt ";
+                    blueLabel.Text = "sleep ";
+                    break;
+                    
+
+                case 10:
+                    outputLabel.Text = "you have a great sleep  ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = " ";
+                    break;
+                    
+
+                case 11:
+                    outputLabel.Text = "you stay up all night and die from exaution ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = " ";
+                    break;
+                    
+
+                case 12:
+                    outputLabel.Text = "you kill the dear and survive \n do you go to the river or stay and serach for dear or sleep  ";
+                    redLabel.Text = "serach ";
+                    blueLabel.Text = "river ";
+                    greenLabel.Text = "sleep";
+                    break;
+                    
+
+                case 13:
+                    outputLabel.Text = "you spot a Sheep. it does not see you yet  what do you do \n sleep or attack ";
+                    redLabel.Text = "sleep ";
+                    blueLabel.Text = "attack ";
+                    greenLabel.Text = "";
+                    break;
+                    
+
+                case 14:
+                    outputLabel.Text = "you have a good nights sleep ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = "  ";
+                    greenLabel.Text = "";
+                    break;
+
+                 case 15:
+                    outputLabel.Text = "you die from the wolf in disguise  ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = "  ";
+                    greenLabel.Text = "";
+                    break;
+
+                 case 16:
+                    outputLabel.Text = "you have a good nights sleep ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = "  ";
+                    greenLabel.Text = "";
+                    break;
+
+                 case 17:
+                    outputLabel.Text = "you spot a Sheep. it does not see you yet what do you do \n attack or sleep ";
+                    redLabel.Text = "attack ";
+                    blueLabel.Text = "sleep ";
+                    greenLabel.Text = "";
+                    break;
+
+                 case 18:
+                    outputLabel.Text = "you have a bad nights sleep with no food  ";
+                    redLabel.Text = "continue ";
+                    blueLabel.Text = " ";
+                    greenLabel.Text = "";
+                    Starve++; 
+                    break;
+
+                 case 19:
+                    outputLabel.Text = "WIP ";
+                    break;
+
+                 case 20:
+                    outputLabel.Text = "the dear gets spooked and runs away do you \n go to the river or look for more dear  ";
+                    redLabel.Text = "river ";
+                    blueLabel.Text = "more dear ";
+                    greenLabel.Text = "";
+                    break;
+
+                 case 99: //loser restart 
+                    outputLabel.Text = "you died do you want to try again ";
+                    redLabel.Text = "yes ";
+                    blueLabel.Text = "no ";
+                    greenLabel.Text = "";
+                    break; 
+
+                 case 100: // win restart 
+                    outputLabel.Text = "you survived do you want to continue to next day ";
+                    redLabel.Text = "yes ";
+                    blueLabel.Text = "no ";
+                    greenLabel.Text = "";
+                    day++;
+                    break;
+
+                 case 101: //closing 
+                    outputLabel.Text = " ";
+                    Thread.Sleep(100);
+                    this.Close();
+                    break;
+                    
+
+            }
+
+        }
+    }
+}
